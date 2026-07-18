e439cde146f599ee210d40d6f3805898d74d3fe0
Robo Tiger <robotiger77@gmail.com>
2026-07-18 23:02:22 +0800

    Add hook assets and game object

---
+++ Grappling Hook/main/hook.go
@@ -0,0 +1,25 @@
+embedded_components {
+  id: "sprite_hook"
+  type: "sprite"
+  data: "default_animation: \"hook\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "sprite_line"
+  type: "sprite"
+  data: "default_animation: \"line\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    y: -19.0
+  }
+}
